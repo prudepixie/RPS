1b1eee01bcd881aa3338f65c96955afa9764e0d3
Wendy Hsu <wndyhsu@gmail.com>
2014-04-03 15:29:32 -0500

    working on terminal client

--- terminal.rb
+++ terminal.rb
@@ -3,29 +3,32 @@ require './lib/rps.rb'
 
 class TerminalClient
   def start
-    #User chooses to sign up - create a new account
+
+    puts "WENDY's Rock-Paper-Scissors Game"
     puts "[1] Sign in"
     puts "[2] Sign up"
 
-    input= gets.chomp
-    if input == "1"
-      puts "What is your username?"
-      username = gets.chomp
-      puts "what is your password?"
-      password = gets.chomp
+    while true
+      input= gets.chomp.to_i
+      break if input =="exit"
+      if input == 1
+        puts "What is your username?"
+        username = gets.chomp
+        puts "what is your password?"
+        password = gets.chomp
 
-      sign_in(username, password)
+        sign_in(username, password)
 
-    elsif input == "2"
-      puts 'create a username for yourself'
-      username = gets.chomp
-      puts 'create a password'
-      password = gets.chomp
+      elsif input == 2
+        puts 'create a username for yourself'
+        username = gets.chomp
+        puts 'create a password'
+        password = gets.chomp
 
-      sign_up(username, password)
+        sign_up(username, password)
 
+      end
     end
-
 
 
 
@@ -33,8 +36,16 @@ class TerminalClient
 
   end
 
+  def commands
+    puts "  users list - List all users"
+    puts "  match list - List active matches"
+    puts "  match play MID - Start playing game with id=MID"
+    puts "  invites - List all pending invites"
+    puts "  invite accept IID - Accept invite with id=IID"
+    puts "  invite create USERNAME - Invite user with username=USERNAME to play a game"
+  end
 
-    def sign_in(username, password)
+  def self.sign_in(username, password)
       result = RPS::SignIn.run(:name =>username, :pw =>password)
       if result.success?
         user = result.user
@@ -45,11 +56,8 @@ class TerminalClient
     end
   end
 
-  def sign_up(username, password)
+  def self.sign_up(username, password)
     result = RPS::SignUp.run(:name => username, :pw => password)
-    # username = "Wendy"
-    # password = "123"
-    # result = RPS::SignUp.run({ :name => "Wendy", :pw => "123" })
     if result.success?
       user = result.user
       puts "#{user.name} is had been added as a new user"
